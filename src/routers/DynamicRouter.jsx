1585bc42f3e047d3a8ac816ebf22cf574ae0b23e
Ziwen Wang <ziwen_wang@pegatroncorp.com>
2024-12-12 15:26:31 -0500

    rollback to lazy load for active kanban page

--- src/routers/DynamicRouter.jsx
+++ src/routers/DynamicRouter.jsx
@@ -3,13 +3,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Loadable from "../components/Loadable";
 import DashboardLayout from "../layout/Dashboard";
 import { useKanbanContext } from "../context/KanbanContext";
-import ActiveKanban from "../pages/activeKanbanPage";
+//import ActiveKanban from "../pages/activeKanbanPage";
 // lazy load Components
 //const TestPage = Loadable(lazy(() => import("../pages/testPage")));
 const LandingPage = Loadable(lazy(() => import("../pages/landingPage")));
 const AddKanbanPage = Loadable(lazy(() => import("../pages/addKanbanPage")));
 const InActiveKanbanPege = Loadable(lazy(()=> import("../pages/inActiveKanbanPage")))
-//const ActiveKanbanPage = Loadable(lazy(()=> import("../pages/activeKanbanPage")))
+const ActiveKanbanPage = Loadable(lazy(()=> import("../pages/activeKanbanPage")))
 
 
 const DynamicRouter = () => {
@@ -20,7 +20,7 @@ const DynamicRouter = () => {
     return routes.map((route) => {
       return {
         path: `/kanban/${route.mapping_key}`,
-        element: <ActiveKanban props={route} />,
+        element: <ActiveKanbanPage props={route} />,
       };
     });
   };
